7baf9b11b3746ed17fd53147de5411164853e7c1
Quinn Caverly <quinncaverly@gmail.com>
2024-09-26 14:25:41 -0400

    init laptop dotfiles

---
+++ fish/config.fish
@@ -0,0 +1,34 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+alias vim='nvim'
+alias perts='cd /mnt/Pertinents/'
+alias aoc='cd /mnt/Pertinents/projects/rust/aoc2023tdd'
+alias ltc='cd /mnt/Pertinents/projects/rust/leetcode'
+
+alias idea='~/Installation/Intellij-Community/bin/idea.sh'
+alias intellij='~/Installation/Intellij-Community/bin/idea.sh'
+alias idea.sh='~/Installation/Intellij-Community/bin/idea.sh'
+
+set -gx PATH $PATH /root/anaconda3/bin/conda
+
+set -gx OPENAI_API_KEY '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'
+set -gx ANTHROPIC_API_KEY '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'
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /home/quinn-caverly/miniconda3/bin/conda
+    eval /home/quinn-caverly/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+else
+    if test -f "/home/quinn-caverly/miniconda3/etc/fish/conf.d/conda.fish"
+        . "/home/quinn-caverly/miniconda3/etc/fish/conf.d/conda.fish"
+    else
+        set -x PATH "/home/quinn-caverly/miniconda3/bin" $PATH
+    end
+end
+# <<< conda initialize <<<
+
+
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
---
+++ nvim/init.lua
@@ -0,0 +1,88 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", -- latest stable release
+        lazypath
+    })
+end
+vim.opt.rtp:prepend(lazypath)
+
+vim.g.mapleader = " "
+
+require("lazy").setup({
+
+    {
+        "EdenEast/nightfox.nvim",
+        lazy = false,    -- make sure we load this during startup if it is your main colorscheme
+        priority = 1000, -- make sure to load this before all the other start plugins
+
+        config = function() vim.cmd("colorscheme nordfox") end
+    }, { "nvim-lualine/lualine.nvim", dependencies = { "nvim-tree/nvim-web-devicons" } }, "nvim-tree/nvim-web-devicons",
+    "nvim-lua/plenary.nvim", "williamboman/mason.nvim", "williamboman/mason-lspconfig.nvim", -- install formatters and language servers using Mason, may need config in null-ls
+
+    "neovim/nvim-lspconfig", "jose-elias-alvarez/null-ls.nvim",                              -- enables formatting, snippets, linting via config in after/
+
+    "nvim-treesitter/nvim-treesitter", "windwp/nvim-autopairs", "folke/trouble.nvim", "nvim-telescope/telescope.nvim",
+    {
+        "numToStr/Comment.nvim",
+        event = "VeryLazy",
+        init = function()
+            vim.o.timeout = true
+            vim.o.timeoutlen = 300
+        end,
+        opts = {
+            -- your configuration comes here
+            -- or leave it empty to use the default settings
+            -- refer to the configuration section below
+        }
+    },
+
+    -- "mfussenegger/nvim-dap", "rcarriga/nvim-dap-ui", "theHamsta/nvim-dap-virtual-text", "nvim-neotest/nvim-nio", "mfussenegger/nvim-dap-python",
+    "Vimjas/vim-python-pep8-indent",
+
+    -- autocompletion with nvim-cmp (has many dependencies)
+    { "VonHeikemen/lsp-zero.nvim", branch = "v3.x" }, { "williamboman/mason.nvim" },
+    { "williamboman/mason-lspconfig.nvim" }, { "neovim/nvim-lspconfig" }, { "L3MON4D3/LuaSnip" },
+    { "hrsh7th/nvim-cmp" }, { "hrsh7th/cmp-nvim-lsp" }, { "hrsh7th/cmp-buffer" }, { "hrsh7th/cmp-path" },
+    { "saadparwaiz1/cmp_luasnip" }, { "rafamadriz/friendly-snippets" },
+    -- fixes global vim problem: does a lot more also ?
+    "folke/neodev.nvim", "ggandor/leap.nvim", "github/copilot.vim", {
+    "nvimdev/dashboard-nvim",
+    event = "VimEnter",
+    config = function() require("dashboard").setup { theme = "hyper" } end,
+    dependencies = { { "nvim-tree/nvim-web-devicons" } }
+}
+})
+
+require("neodev").setup()
+
+local lsp_zero = require("lsp-zero")
+lsp_zero.extend_lspconfig()
+lsp_zero.on_attach(function(_, bufnr) lsp_zero.default_keymaps({ buffer = bufnr }) end)
+
+lsp_zero.set_sign_icons({ error = "✘", warn = "▲", hint = "⚑", info = "»" })
+
+require("mason").setup()
+require("mason-lspconfig").setup({
+    handlers = {
+        lsp_zero.default_setup,
+        lua_ls = function()
+            local lua_opts = lsp_zero.nvim_lua_ls()
+            require("lspconfig").lua_ls.setup(lua_opts)
+        end
+    }
+})
+
+require("lspconfig").lua_ls.setup({})
+require("cmp").setup({})
+
+-- require("nvim-dap-virtual-text").setup({})
+-- require("dapui").setup()
+-- require("dap-python").setup("~/.virtualenvs/debugpy/bin/python")
+-- require('dap-python').test_runner = 'pytest'
+
+require("nvim-autopairs").setup()
+require "nvim-web-devicons".setup()
+require("leap").create_default_mappings()
+
+require("quinn-caverly")
